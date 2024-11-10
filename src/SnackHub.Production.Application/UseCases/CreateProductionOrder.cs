48f775c1b08ea3085b4c06ebfc7f9ad97c574b3e
Kássio San <kciosantana@gmail.com>
2024-11-10 10:21:14 -0300

    Should_Create_Production_Order_Async (GREEN)

--- src/SnackHub.Production.Application/UseCases/CreateProductionOrder.cs
+++ src/SnackHub.Production.Application/UseCases/CreateProductionOrder.cs
@@ -13,35 +13,15 @@ public class CreateProductionOrder(
     public async Task<CreateKitchenOrderResponse> Execute(CreateProductionOrderRequest request)
     {
         var response = new CreateKitchenOrderResponse();
-        var order = new Order(default, []);
 
-        if (order is null)
-        {
-            response.AddNotification(nameof(request.OrderId), "Order not found");
-            return response;
-        }
+        //var items = order
+        //    .Items
+        //    .Select(orderItem => ProductionOrderItem.Factory.Create(orderItem.ProductName, orderItem.Quantity))
+        //    .ToList();
 
-        if (order.Status is not OrderStatus.Accepted)
-        {
-            response.AddNotification(nameof(request.OrderId), "Only confirmed orders can be send to the kitchen");
-            return response;
-        }
+        await _productionOrderRepository.AddAsync(new Domain.Entities.ProductionOrder(request.OrderId, []));
 
-        try
-        {
-            var items = order
-                .Items
-                .Select(orderItem => ProductionOrderItem.Factory.Create(orderItem.ProductName, orderItem.Quantity))
-                .ToList();
+        return response;
 
-            await _productionOrderRepository.AddAsync(new Domain.Entities.ProductionOrder(order.Id, items));
-
-            return response;
-        }
-        catch (Exception exception)
-        {
-            response.AddNotification(nameof(request.OrderId), exception.Message);
-            return response;
-        }
     }
 }
