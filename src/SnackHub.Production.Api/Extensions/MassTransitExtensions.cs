0b7df5cb7534c4dce6c1dfe7aa25fd069c715386
Icaro Sousa <icaro.py@gmail.com>
2024-11-11 00:44:56 +0000

    fix: simplifying message replication settings;

--- src/SnackHub.Production.Api/Extensions/MassTransitExtensions.cs
+++ src/SnackHub.Production.Api/Extensions/MassTransitExtensions.cs
@@ -13,6 +13,7 @@ public static class MassTransitExtensions
         
         serviceCollection.AddMassTransit(busConfigurator =>
         {
+            busConfigurator.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("production-service"));
             busConfigurator.AddConsumer<ProductCreatedConsumer>();
             busConfigurator.AddConsumer<ProductUpdatedConsumer>();
             busConfigurator.AddConsumer<ProductDeletedConsumer>();
@@ -20,14 +21,6 @@ public static class MassTransitExtensions
             busConfigurator.SetKebabCaseEndpointNameFormatter();
             busConfigurator.UsingRabbitMq((context, configurator) =>
             {
-                // Shared messages - those will be in a replicated queue at RabbitMQ
-                configurator.ReceiveEndpoint("production-service", rabbitMqReceiveEndpointConfigurator =>
-                {
-                    rabbitMqReceiveEndpointConfigurator.ConfigureConsumer<ProductCreatedConsumer>(context);
-                    rabbitMqReceiveEndpointConfigurator.ConfigureConsumer<ProductUpdatedConsumer>(context);
-                    rabbitMqReceiveEndpointConfigurator.ConfigureConsumer<ProductDeletedConsumer>(context);
-                });
-                
                 configurator.Host(settings.Host, "/",  rabbitMqHostConfigurator =>
                 {
                     rabbitMqHostConfigurator.Username(settings.UserName);
